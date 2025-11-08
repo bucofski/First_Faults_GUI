843f33ee248aa71dee470d34e678ebcebaaafa7b
TVDV <67153535+bucofski@users.noreply.github.com>
2025-11-08 12:02:32 +0100

    test co,,it

--- data/model/ews.py
+++ data/model/ews.py
@@ -2,3 +2,9 @@ class EWS:
     def __init__(self, name, url):
         self.name = name
         self.url = url
+
+    def get_name(self):
+        return self.name
+
+    def get_url(self):
+        return self.url
